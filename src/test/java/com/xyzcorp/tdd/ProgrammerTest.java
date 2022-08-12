11680de63891f2bd0160003cf31a15de9f9fc95b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-08-12 17:04:28 +0000

    Added a single assertion

--- src/test/java/com/xyzcorp/tdd/ProgrammerTest.java
+++ src/test/java/com/xyzcorp/tdd/ProgrammerTest.java
@@ -1,5 +1,14 @@
 package com.xyzcorp.tdd;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class ProgrammerTest {
 
+    
+    @Test
+    public void testSomething() {
+        assertThat("1").isEqualTo("1");
+    }
 }
