cbcca8bd92b1d62ce139f21c13fedb9a61341956
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-08-12 17:13:02 +0000

    Added a new test for programmer

--- src/main/java/com/xyzcorp/tdd/Programmer.java
+++ src/main/java/com/xyzcorp/tdd/Programmer.java
@@ -1,5 +1,25 @@
 package com.xyzcorp.tdd;
 
+import java.time.LocalDate;
+import java.util.function.Supplier;
+
 public class Programmer {
+
+    private String firstName;
+    private LocalDate birthDate;
+    private Supplier<LocalDate> localDateSupplier;
+    private String lastName;
+
+    public Programmer(String firstName, String lastName, 
+                      LocalDate birthDate, Supplier<LocalDate> localDateSupplier) {
+                        this.firstName = firstName;
+                        this.lastName = lastName;
+                        this.birthDate = birthDate;
+                        this.localDateSupplier = localDateSupplier;                
+    }
+
+    public Object getFullName() {
+        return firstName + " " + lastName;
+    }
 
 }
--- src/test/java/com/xyzcorp/tdd/ProgrammerTest.java
+++ src/test/java/com/xyzcorp/tdd/ProgrammerTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDate;
+
 public class ProgrammerTest {
 
     
     @Test
-    public void testSomething() {
-        assertThat("1").isEqualTo("1");
+    public void testFullName() {
+        Programmer programmer = new Programmer(
+            "Bjarne", "Strousoup", LocalDate.of(1950, 12, 30),
+             () -> LocalDate.of(2018, 6, 1));
+         assertThat(programmer.getFullName()).isEqualTo("Bjarne Strousoup");
     }
 }
